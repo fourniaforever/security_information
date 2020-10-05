44f62bc46913f15daa1ed9dbcd4df9baec7c20fe
seikosan <ionova_dasha123@mail.ru>
2020-10-05 11:10:08 +0400

    First task and a little bit of second

---
+++ task 1/InformationSecurityTask1/InformationSecurityTask1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InformationSecurityTask1
+{
+    class Program
+    {
+        static string path = @"C:\Users\fourn\OneDrive\Рабочий стол\sign"; 
+
+        static byte[] GetFileSignature(string filename, int offset, int size) 
+        {
+            var fileInfo = new FileInfo(filename);
+            var reader = fileInfo.OpenRead(); 
+            var buffer = new byte[size];
+
+            reader.Seek(offset, SeekOrigin.Begin);
+            reader.Read(buffer, 0, size);
+
+            return buffer;
+        }
+
+        public static void SearchFiles(string dir, byte[] bytes)
+        {
+            var result = new List<string>();
+            if (Directory.GetDirectories(dir).Any())
+            {
+                foreach (var item in Directory.GetDirectories(dir))
+                {
+                    SearchFiles(item, bytes);
+                }
+            }
+            string[] files = Directory.GetFiles(dir);
+            if (files.Any())
+            {
+                foreach (var file in files)
+                {
+                  Console.WriteLine(file);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Name of file:");
+            string filename = Console.ReadLine();
+
+            Console.Write("Offset:");
+            int offset = int.Parse(Console.ReadLine()); 
+
+            Console.Write("Size of signature:");
+            int size = int.Parse(Console.ReadLine());
+
+            byte[] filesignature = GetFileSignature(filename, offset, size);
+
+            SearchFiles(path, filesignature);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ task 2/InformationSecurity2/InformationSecurity2/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace InformationSecurity2
+{
+    class Program
+    {
+        public static void RebuildContainer(string originFileName, string containerFileName)
+        {
+            using (StreamReader streamReader = new StreamReader(originFileName))
+            {
+                using (StreamWriter streamWriter = new StreamWriter(containerFileName, false, Encoding.ASCII))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        streamWriter.WriteLine(streamReader.ReadLine());
+                    }
+                }
+            }
+        }
+        public static void Encryption(string inputFileName, string containerFileName)
+        {
+            RebuildContainer("origin.txt", containerFileName);
+            string inputData = "";  //кодируемая информация 
+            using (StreamReader streamReader = new StreamReader(inputFileName, Encoding.ASCII))
+            {
+                inputData = streamReader.ReadToEnd();
+            }
+            string textOfContainer = "";  //контейнер
+            using (StreamReader streamReader1 = new StreamReader(containerFileName, Encoding.ASCII))
+            {
+                textOfContainer = streamReader1.ReadToEnd();
+            }
+            using (StringReader stringReader = new StringReader(textOfContainer))
+            {
+                using (StreamWriter streamWriter = new StreamWriter(containerFileName, false, Encoding.ASCII))
+                {
+                    foreach (var symbol in inputData)
+                    {
+                        byte[] bytes = BitConverter.GetBytes(symbol).Take(1).ToArray();
+                        BitArray bits = new BitArray(bytes);
+                        foreach (bool bit in bits)
+                        {
+                            string line = stringReader.ReadLine();
+                            if (bit)
+                            {
+                                line += " ";
+                            }
+                            streamWriter.WriteLine(line);
+                        }
+                    }
+                    string remainder = stringReader.ReadLine();
+                    while (remainder != null)
+                    {
+                        streamWriter.WriteLine(remainder);
+                        remainder = stringReader.ReadLine();
+                    }
+                }
+            }
+        }
+        public static void Decryption(string containerFileName, string outputFileName)
+        {
+            using (StreamReader streamReader = new StreamReader(containerFileName, Encoding.ASCII))
+            {
+                using (StreamWriter streamWriter = new StreamWriter(outputFileName, false, Encoding.ASCII))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        BitArray bits = new BitArray(8, false);
+                        for (int i = 0; i < bits.Length; i++)
+                        {
+                            string line = streamReader.ReadLine();
+                            if (!streamReader.EndOfStream && line.Last() == ' ')
+                            {
+                                bits[i] = true;
+                            }
+                            else if (streamReader.EndOfStream)
+                            {
+                                break;
+                            }
+                        }
+                        if (!bits.Equals(new BitArray(8, false)))
+                        {
+                            byte[] bytes = new byte[1];
+                            bits.CopyTo(bytes, 0);
+                            streamWriter.Write(Convert.ToChar(bytes[0]).ToString());
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("1 - Шифрование");
+            Console.WriteLine("2 - Дешифрование");
+            if (int.TryParse(Console.ReadLine(), out var action) && (action == 1 || action == 2))
+            {
+                if (action == 1)
+                {
+                    Encryption("input.txt", "songContainer.txt");
+                }
+                if (action == 2)
+                {
+                    Decryption("songContainer.txt", "output.txt");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректное действие!");
+            }
+        }
+    }
+}
